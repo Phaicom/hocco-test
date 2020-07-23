74cbd95b326bab7205681fd11f95e0db66b91de5
Reawpai Chunsoi <reawpai.chun@yahoo.com>
2020-07-23 16:02:02 +0700

    Delete obsolete action

--- store/actions.js
+++ store/actions.js
@@ -1,10 +1,4 @@
 export default {
-  async GET_USERS({}) {
-    return await this.$axios.get(`${process.env.USERS_ENDPOINT}`)
-  },
-  async GET_COMMENTS({}) {
-    return await this.$axios.get(`${process.env.COMMENTS_ENDPOINT}`)
-  },
   async GET_SPECIES({}) {
     return await this.$axios.get(`${process.env.SPECIES_ENDPOINT}`)
   },
